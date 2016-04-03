873d1e7605b9ea5ca94f853b4b81ab7c400c6ebe
lananhdo1 <nguoibannho93@gmail.com>
2016-04-03 03:47:58 +0000

    type your commit message here

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -11,8 +11,8 @@ class IdeasController < ApplicationController
   # GET /ideas/1.json
   def show
     
-@comments = @idea.comments.all
-@comment = @idea.comments.build
+   @comments = @idea.comments.order(like_count: :desc).all
+    @comment = @idea.comments.build
   end
 
   # GET /ideas/new
